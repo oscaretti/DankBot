66c8d45ef5d5f205aaa4358bf1dbbdccff76022e
Oscaretti <oscaretti@gmail.com>
2018-06-16 03:12:01 +0200

    Added the rest of the required commands and fixed argument parsing

--- main.py
+++ main.py
@@ -4,33 +4,31 @@ from discord.ext import commands
 import asyncio
 import datetime
 import pickle
+import random
 
 VERSION = "0.1"
 
-manage_roles = {}   # List of role id's that are allowed to manage the bot
-token = ""          # Your bot token goes here
-prefix = "%"        # Your preferred command prefix
+manage_roles = {"457241869680902157"}  # List of role id's that are allowed to manage the bot
+token = "NDU3MjMwMzMzNDYxNTI4NTc2.DgXfsg.V3aPIgNqkIt_Xg0f2PVlLqKhSAA"  # Your bot token goes here
+prefix = "%"  # Your preferred command prefix
 
 Client = discord.Client()
 bot = commands.Bot(command_prefix=prefix)
 allowed_channels = {"440720123448393728"}
-timeout = datetime.datetime()
-user_timeouts = dict()
-user_score = dict()
 
 
 # Verifies if the role list of the user has a role that is allowed to manage the bot
 def can_manage(roles):
     for role in roles:
         for manage_role in manage_roles:
-            if role == manage_role:
+            if role.id == manage_role:
                 return True
     return False
 
 
 # 3 simple methods for the sake of code clarity
 def on_timeout():
-    return datetime.datetime.now() < timeout
+    return datetime.datetime.now() < bot_timeout
 
 
 def user_on_timeout(user_id):
@@ -41,48 +39,50 @@ def user_on_timeout(user_id):
 
 # The bot stores data between sessions in a data.data file in the scripts repository
 def save():
-    file = open("data.data", "w")
-    pickle.dump([allowed_channels, timeout, user_timeouts, user_score], file)
+    file = open("data.data", "wb")
+    pickle.dump([allowed_channels, bot_timeout, user_timeouts, user_score,
+                 user_cooldown, bot_cooldown, bot_cooldown_deviation], file)
 
 
 @bot.event
 async def on_ready():
-    global allowed_channels, timeout, user_timeouts, user_score
+    global allowed_channels, bot_timeout, user_timeouts, user_score, user_cooldown, bot_cooldown, bot_cooldown_deviation
     try:
-        file = open("data.data", "r")
-        allowed_channels, timeout, user_timeouts, user_score = pickle.load(file)
-    except IOError:
+        file = open("data.data", "rb")
+        allowed_channels, bot_timeout, user_timeouts, user_score, \
+            user_cooldown, bot_cooldown, bot_cooldown_deviation = pickle.load(file)
+    except Exception:
         print("No data file appears to exist, initializing variables")
-        allowed_channels = []
-        timeout = datetime.datetime.now()
+        allowed_channels = set()
+        bot_timeout = datetime.datetime.now()
         user_timeouts = dict()
-        user_timeouts = dict()
-print("Bot is ready!")
+        user_score = dict()
+        user_cooldown = datetime.timedelta(minutes=1)
+        bot_cooldown = datetime.timedelta(minutes=10)
+        bot_cooldown_deviation = 0.15
+    print("Bot is ready!")
 
 
 # Management commands
-@bot.command(name="allow")
-async def bot_allow(message):
-    if message.author == bot:
-        return
-    cont = str(message.content)
-    if message.author.id in manage_roles:
+@bot.command(name="allow", pass_context=True)
+async def bot_allow(context):
+    cont = str(context.message.content)
+    if can_manage(context.message.author.roles):
         args = cont.split(sep=" ")
         try:
             x = args[1]
         except IndexError:
-            x = message.channel.id
+            x = context.message.channel.id
         allowed_channels.add(x)
         await bot.say("Added channel <#%s> to the allowed list" % x)
     else:
         await bot.say("You do not have the permission to use that command")
 
 
-@bot.command(name="shutdown")
-async def bot_shutdown(message):
-    if message.author == bot:
-        return
-    if message.author.id in manage_roles:
+@bot.command(name="shutdown", pass_context=True)
+async def bot_shutdown(context):
+    if can_manage(context.message.author.roles):
+        save()
         await bot.say("Shutting down...")
         await bot.logout()
     else:
@@ -91,9 +91,49 @@ async def bot_shutdown(message):
 
 # All user commands
 @bot.command(name="info")
-async def bot_info(message):
-    if message.author == bot:
-        return
+async def bot_info():
     await bot.say("DankBot %s (https://github.com/oscaretti/DankBot)" % VERSION)
+
+
+@bot.command(name="dank", pass_context=True)
+async def bot_dank(context):
+    if context.message.channel.id not in allowed_channels:
+        return
+    user = context.message.author.id
+    if user_on_timeout(user):
+        wait_time = user_timeouts[user] - datetime.datetime.now()
+        await bot.say("Not so fast <@%s>, you have to wait %s to try again" % (user, str(wait_time)))
+    elif on_timeout():
+        new_user_timeout = datetime.datetime.now() + user_cooldown
+        await bot.say("Someone has just said that <@%s>! Try again in %s" % (user, user_cooldown))
+        user_timeouts[user] = new_user_timeout
+    else:
+        if user in user_score:
+            user_score[user] += 1
+        else:
+            user_score[user] = 1
+        # Setting new timeout date
+        now = datetime.datetime.now()
+        bottom_range = now + (bot_cooldown - bot_cooldown_deviation * bot_cooldown)
+        top_range = now + (bot_cooldown + bot_cooldown_deviation * bot_cooldown)
+        random.seed()
+        span = top_range - bottom_range
+        span *= random.random()
+        global bot_timeout
+        bot_timeout = bottom_range + span
+        save()
+        await bot.say("It is pretty dank my dude. You have %i points so far <@%s>" % (user_score[user], user))
+
+
+@bot.command(name="score", pass_context=True)
+async def bot_score(context):
+    if context.message.channel.id not in allowed_channels:
+        return
+    user = context.message.author.id
+    if user in user_score:
+        await bot.say("Your score is %i <@%s>" % (user_score[user], user))
+    else:
+        await bot.say("You don't have any score yet <@%s>" % user)
+
 
 bot.run(token)
